bcf19c43d1c5fcebf5f4c7aa348b433e2d9392db
Lucas Magalhães <imlucasmagalhaes@outlook.com>
2023-11-20 13:12:56 -0300

    creating array

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const height = 3
 
 function start(){
     createArray()
-    console.log(array)
+    renderArray()
 }
 
 //cria o meu array
@@ -16,8 +16,29 @@ function createArray(){
     }
 }
 
+//desenha o meu array
 function renderArray(){
+    let html = '<table cellpadding = 0 cellspacing = 0>'
 
+    for(let row = 0; row < height; row++){
+        html += '<tr>'
+
+        for(let column = 0; column < width; column++){
+            //converte um vetor de linha e coluna, e transforma em um vetor unidirecional
+            const pixelIndex = column + (width * row)
+             
+            html += '<td>'
+            html += pixelIndex
+            html += '</td>'
+        }
+
+        html += '</tr>'
+    }
+
+    html += '</table>'
+
+
+    document.querySelector('#field').innerHTML = html
 }
 
 start()
